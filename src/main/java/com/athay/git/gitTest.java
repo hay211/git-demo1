d1313d8315ffb6da069ef62d6d8cc8ab88eaaad1
hay <2112568986@qq.com>
2023-05-11 20:13:37 +0800

    first commit

---
+++ src/main/java/com/athay/git/gitTest.java
@@ -0,0 +1,14 @@
+package com.athay.git;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: hay start
+ * @Date: 2023/05/11/20:09
+ * @Description:
+ */
+public class gitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,git");
+    }
+}
